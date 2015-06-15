94cd1b9cf02a9e43be267144bc4c725c49cedca5
Arun Prakash <arunslb123@gmail.com>
2015-06-15 12:46:53 +0530

    Create payment.tpl.php

---
+++ payment.tpl.php
@@ -0,0 +1,165 @@
+    <div class="container">
+        <div class="row no-margin" id="toprow">
+            <div class="col-xs-10 col-xs-offset-3 no-padding">
+                <div class="panel-group" id="accordion">
+                    <div class="panel">
+                        <div class="panel-heading header-color" data-parent=
+                        "#accordion" data-target="#collapseOne" data-toggle=
+                        "collapse">
+                            <h4 class="panel-title"><a class=
+                            "accordion-toggle">1. Before you place your
+                            order&gt; Sign In</a></h4>
+                        </div>
+
+                        <div class="panel-collapse collapse in" id=
+                        "collapseOne">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-xs-5 col-xs-offset-2">
+                                        Sign in with your social account<br>
+                                        <span style="font-size:10px">&nbsp;
+                                        &nbsp;(No posts on your behalf,
+                                        promise.)</span><br>
+                                        <a class=
+                                        "btn btn-block btn-social btn-facebook">
+                                        <i class="fa fa-facebook"></i>
+                                        Facebook</a><br>
+                                        <a class=
+                                        "btn btn-block btn-social btn-google"><i class="fa fa-google-plus">
+                                        </i> Google</a>
+                                    </div>
+
+                                    <div class="col-xs-6 col-xs-offset-2" id=
+                                    "emailpart">
+                                        What is your Email Address?<br>
+                                        <input class="form-control" id=
+                                        "InputEmail" placeholder="Enter email"
+                                        type="email"><br>
+                                        <button class="btn btn-success" id=
+                                        "emailbutton" type=
+                                        "button">Continue</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel">
+                        <div class="panel-heading header-color">
+                            <h4 class="panel-title" data-parent="#accordion"
+                            data-target="#collapseTwo" data-toggle="collapse">
+                            <a class="accordion-toggle">2. Delivery
+                            Address</a></h4>
+                        </div>
+
+                        <div class="panel-collapse collapse" id="collapseTwo">
+                            <div class="panel-body">
+                                <form class="form-horizontal">
+                                    <div class="form-group">
+                                        <label class="col-xs-2 control-label"
+                                        for="inputName1">Name</label>
+
+                                        <div class="col-xs-10">
+                                            <input class="form-control" id=
+                                            "inputName1" type="text">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-xs-2 control-label"
+                                        for="inputPincode1">Pincode</label>
+
+                                        <div class="col-xs-10">
+                                            <input class="form-control" id=
+                                            "inputPincode1" type="text">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-xs-2 control-label"
+                                        for="inputAddress1">Address</label>
+
+                                        <div class="col-xs-10">
+                                            <textarea class="form-control" id=
+                                            "inputAddress1" rows="5">
+</textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-xs-2 control-label"
+                                        for="inputPhone1">Phone Number</label>
+
+                                        <div class="col-xs-10">
+                                            <input class="form-control" id=
+                                            "inputPhone1" type="text">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-xs-offset-2 col-xs-10">
+                                            <button class="btn btn-success"
+                                            type="submit">Save &
+                                            Continue</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel">
+                        <div class="panel-heading header-color">
+                            <h4 class="panel-title" data-parent="#accordion"
+                            data-target="#collapseThree" data-toggle=
+                            "collapse"><a class="accordion-toggle">3. Order
+                            Summary</a></h4>
+                        </div>
+
+                        <div class="panel-collapse collapse" id=
+                        "collapseThree">
+                            <div class="panel-body">
+                                <div class="row"  id="ordersummary">
+                                    <div class="col-xs-2 checkbox" id="checkboxdetails" >
+                                              <input type="checkbox" id="checkvalue">
+                                   </div>
+                                    <div class="col-xs-3 col-xs-offset-1" id="imagecart"><b>Product</b></div>
+                                    <div class="col-xs-3 col-xs-offset-1" id="productname"><b>Product Name</b></div>
+                                    <div class="col-xs-3 col-xs-offset-1" id="productprice"><b>Product Price</b></div>
+
+                                </div>
+                                <br>
+                                <div class="row" id="paymentupdate">
+                                <br>
+                                     <div class="col-xs-3 col-xs-offset-1" id-"continuebutton">
+                                       <button class="btn btn-success" id="3continue"
+                                            type="submit">
+                                            Continue</button>
+                                     </div>
+                                     <div class="col-xs-4 col-xs-offset-8" id="amountdisplay">
+
+                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel">
+                        <div class="panel-heading header-color">
+                            <h4 class="panel-title" data-parent="#accordion"
+                            data-target="#collapseFour" data-toggle="collapse">
+                            <a class="accordion-toggle">4. Payment
+                            Summary</a></h4>
+                        </div>
+
+                        <div class="panel-collapse collapse" id="collapseFour">
+                            <div class="panel-body">
+                                Text
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
